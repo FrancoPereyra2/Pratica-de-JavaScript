d253bf71eaa9e0171a167f81cf18f5b5a9b59546
Franco Pereyra <francopereyra325@gmail.com>
2024-07-11 15:35:15 -0300

    usamos do while

--- js/bucles.js
+++ js/bucles.js
@@ -1,5 +1,18 @@
-let numero = 1
+let numero = 100;
 while (numero <= 10) {
-    document.write(`<p> Linea numero ${numero}</p>`)
-    numero = numero + 1
+  document.write(`<p> Linea numero ${numero}</p>`);
+  numero = numero + 1;
 }
+
+/*
+do{
+todas las lineas de codigo que quiero repetir
+agregar una linea que haga la condicion logica se deje de cumplir
+}while(condicion logica)
+*/
+
+let renglon = 1;
+do {
+  document.write(`<p> Renglon numero ${renglon}</p>`);
+   renglon++; //renglon = renglon + 1;
+} while (renglon <= 5);
