2df8290b903ec13ac5bc8de2698c95b6b7e9fa7e
Franco Pereyra <francopereyra325@gmail.com>
2024-07-23 16:31:54 -0300

    vimos un poco de objetos

---
+++ js/07-objetos.js
@@ -0,0 +1,11 @@
+// notacion literal
+
+const vuelve = {
+    // propiedades
+    artista: 'Ricky Martin',
+    titulo: 'Vuelve',
+    genero: ['balada', 'pop'],
+    anio: 1998,
+    autor: 'franco de Vita'
+    //metodos
+}
